5312bd5e630f6f925d0e34867456b384cd9c242d
Dcnigma <bart_is@hotmail.com>
2025-11-06 05:49:01 +0100

    Update global.cpp

--- global.cpp
+++ global.cpp
@@ -8,8 +8,3 @@ unsigned char SRAM[SRAM_SIZE];
 
 int gRomSet = 0;
 
-float getTime() {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return ts.tv_sec * 1000.f + ts.tv_nsec / 1000000.f; // milliseconds
-}
