2de920a0ac2d9fc4356153f2304ce714161e34b8
Dcnigma <bart_is@hotmail.com>
2025-11-06 04:24:57 +0100

    Update mmDisplay.h

--- mmDisplay.h
+++ mmDisplay.h
@@ -1,33 +1,26 @@
-#ifndef MMDISPLAY_H
-#define MMDISPLAY_H
+#pragma once
+#include <stdint.h>
+#include <EGL/egl.h>
+#include <glad/glad.h>
 
-#include <cstdint>
+typedef unsigned short WORD;
 
-class mmDisplay
-{
+class mmDisplay {
 public:
     mmDisplay();
     ~mmDisplay();
 
-    uint16_t m_Width;
-    uint16_t m_Height;
-    uint16_t m_Depth;
-
-    uint32_t m_FrameCount;
-    uint16_t m_ViewWidth;
-    uint16_t m_ViewHeight;
-
-    bool m_IsOpen;
-
-    // Functions
+    int Open(uint16_t Width, uint16_t Height);
+    void Close();
     bool RenderScene();
     void ClearScreen();
     void BeginScene();
     void EndScene();
-    void Close();
     void UpdateScreenBuffer(unsigned char* source);
     void MakeScreenBuffer();
-    int Open(uint16_t Width, uint16_t Height);
+
+private:
+    WORD* IntermediateBuffer;
 };
 
-#endif // MMDISPLAY_H
+extern mmDisplay theDisplay;
