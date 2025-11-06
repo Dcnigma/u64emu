8a0f1d0f9561d4f32acc3ba22d115edf77d0840f
Dcnigma <bart_is@hotmail.com>
2025-11-06 04:39:38 +0100

    Update mmDisplay.h

--- mmDisplay.h
+++ mmDisplay.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <stdint.h>
+#include <switch.h>
 #include <EGL/egl.h>
 #include <glad/glad.h>
 
@@ -13,14 +13,20 @@ public:
     int Open(uint16_t Width, uint16_t Height);
     void Close();
     bool RenderScene();
+    void UpdateScreenBuffer(unsigned char* source);
+    void MakeScreenBuffer();
     void ClearScreen();
     void BeginScene();
     void EndScene();
-    void UpdateScreenBuffer(unsigned char* source);
-    void MakeScreenBuffer();
 
 private:
-    WORD* IntermediateBuffer;
+    static EGLDisplay s_display;
+    static EGLContext s_context;
+    static EGLSurface s_surface;
+    static GLuint s_program;
+    static GLuint VBO, VAO, EBO;
+    static GLuint s_tex;
 };
 
+// Declare global display object
 extern mmDisplay theDisplay;
