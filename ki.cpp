aafca7123720c43e32a8b005daf12c479037f8b0
Dcnigma <bart_is@hotmail.com>
2025-11-06 19:38:59 +0100

    Update ki.cpp
    Live loop for KI boot

--- ki.cpp
+++ ki.cpp
@@ -2,18 +2,33 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <cstdlib>
 
+// --- Globals ---
 bool bQuitSignal = false;
+u32 gRomSet = 0;
+u32 gAllowHLE = 0;
 
 // --- Minimal CKIApp stub ---
 class CKIApp {
 public:
+    u32 m_UCode;
+    u32 m_BootCode;
+    u32 m_DIPS;
+    bool m_LockOn;
+    u32 m_FrameDelay;
+    u32 m_VTraceInc;
+    char m_HDImage[64];
+    char m_ARom1[32], m_ARom2[32], m_ARom3[32], m_ARom4[32];
+    char m_ARom5[32], m_ARom6[32], m_ARom7[32], m_ARom8[32];
+
     void LogMessage(const char* fmt, ...);
     void ErrorMessage(const char* fmt, ...);
 };
 
 CKIApp theApp;
 
+// --- Logging ---
 void CKIApp::LogMessage(const char* fmt, ...)
 {
     FILE* f = fopen("log.txt", "a");
@@ -39,46 +54,166 @@ void CKIApp::ErrorMessage(const char* fmt, ...)
     fclose(f);
 }
 
-// --- MAIN ---
-int main(int argc, char* argv[])
+// --- Dummy emulator object ---
+class CEmuObject {
+public:
+    bool initialized;
+    CEmuObject() : initialized(false) {}
+
+    bool Init()
+    {
+        printf("CEmuObject::Init() called\n");
+        fflush(stdout);
+        initialized = true;
+
+        // Allocate a small memory buffer to simulate ROM/RAM setup
+        void* testBuffer = malloc(1024);
+        if (testBuffer)
+        {
+            memset(testBuffer, 0xAA, 1024);
+            free(testBuffer);
+            printf("Memory test passed\n");
+            fflush(stdout);
+            return true;
+        }
+        printf("Memory allocation failed!\n");
+        return false;
+    }
+
+    void Shutdown()
+    {
+        if (initialized)
+        {
+            printf("CEmuObject::Shutdown() called\n");
+            initialized = false;
+        }
+    }
+};
+
+// --- BootKI1 (running version) ---
+void BootKI1(void)
 {
-    consoleInit(NULL);
-    printf("Hello from CKIApp minimal test!\n");
-    printf("Press + to exit.\n");
+    printf("BootKI1(): setting up emulator structures...\n");
     fflush(stdout);
 
-    // Simple log test
-    theApp.LogMessage("Program started successfully");
+    theApp.m_UCode = 0;
+    theApp.m_BootCode = 0x3F;
+    theApp.m_DIPS = 3584;
+    theApp.m_LockOn = true;
+    theApp.m_FrameDelay = (uint32_t)(1000.f / 60.f + 0.3f);
+    theApp.m_VTraceInc = 49999980 / 60;
 
-#ifdef NXOVERCLOCK
-    if (R_SUCCEEDED(pcvInitialize()))
+    gAllowHLE = 1;
+    bQuitSignal = false;
+
+    strcpy(theApp.m_HDImage, "ki.img");
+    strcpy(theApp.m_ARom1, "U10-L1");
+    strcpy(theApp.m_ARom2, "U11-L1");
+    strcpy(theApp.m_ARom3, "U12-L1");
+    strcpy(theApp.m_ARom4, "U13-L1");
+    strcpy(theApp.m_ARom5, "U33-L1");
+    strcpy(theApp.m_ARom6, "U34-L1");
+    strcpy(theApp.m_ARom7, "U35-L1");
+    strcpy(theApp.m_ARom8, "U36-L1");
+
+    gRomSet = 1; // KI1
+
+    printf("BootKI1(): setup done.\n");
+    fflush(stdout);
+
+    CEmuObject e;
+    if (!e.Init())
     {
-        pcvSetClockRate(PcvModule::PcvModule_Cpu, 1785000000);
-        printf("CPU overclocked\n");
+        printf("BootKI1(): emulator initialization failed!\n");
         fflush(stdout);
+        return;
     }
-#endif
 
-    // Main loop
-    while (appletMainLoop())
+    printf("BootKI1(): emulator initialized successfully.\n");
+    fflush(stdout);
+
+    // --- Minimal emulation loop ---
+    PadState pad;
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+    padInitializeDefault(&pad);
+
+    bool emuRunning = true;
+    while (appletMainLoop() && emuRunning)
     {
-        hidScanInput();
-        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+        padUpdate(&pad);
+        u64 kDown = padGetButtonsDown(&pad);
 
-        if (kDown & KEY_PLUS)
+        if (kDown & HidNpadButton_Plus)
         {
-            printf("Exiting...\n");
+            printf("BootKI1(): Exiting emulation...\n");
             fflush(stdout);
-            break;
+            emuRunning = false;
         }
+
+        // Here you could call your CEmuObject update functions:
+        // e.UpdateDisplay();
+        // svcSleepThread(16000); // ~16ms for 60fps
 
         consoleUpdate(NULL);
     }
 
-#ifdef NXOVERCLOCK
-    pcvSetClockRate(PcvModule::PcvModule_Cpu, 1020000000);
-    pcvExit();
-#endif
+    // Clean shutdown
+    e.Shutdown();
+    printf("BootKI1(): exited cleanly.\n");
+    fflush(stdout);
+}
+
+
+// --- BootKI2 stub ---
+void BootKI2(void)
+{
+    printf("BootKI2(): Not implemented yet.\n");
+    fflush(stdout);
+}
+
+// --- Main ---
+int main(int argc, char* argv[])
+{
+    consoleInit(NULL);
+    printf("Killer Instinct Switch Loader\n");
+    printf("------------------------------\n");
+    printf("A = Boot KI (ki.img)\n");
+    printf("B = Boot KI2 (ki2.img) [not yet implemented]\n");
+    printf("+ = Exit\n");
+    fflush(stdout);
+
+    PadState pad;
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+    padInitializeDefault(&pad);
+
+    bool running = true;
+
+    while (appletMainLoop() && running)
+    {
+        padUpdate(&pad);
+        u64 kDown = padGetButtonsDown(&pad);
+
+        if (kDown & HidNpadButton_Plus)
+        {
+            printf("Exiting...\n");
+            fflush(stdout);
+            running = false;
+        }
+        else if (kDown & HidNpadButton_A)
+        {
+            printf("Calling BootKI1()...\n");
+            fflush(stdout);
+            BootKI1();
+        }
+        else if (kDown & HidNpadButton_B)
+        {
+            printf("Calling BootKI2()...\n");
+            fflush(stdout);
+            BootKI2();
+        }
+
+        consoleUpdate(NULL);
+    }
 
     consoleExit(NULL);
     return 0;
